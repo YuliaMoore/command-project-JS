12b35270a5a38963e232dea6c03ed39fa7c6d3cb
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-05-07 13:44:59 +0000

    Deploying to gh-pages from @ YuliaMoore/command-project-JS@a1bf75593e97f8bd105ec7e568f02d009e84ed87 🚀

---
+++ catalog.4d095a64.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")};e.openMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}))})();
+//# sourceMappingURL=catalog.4d095a64.js.map
