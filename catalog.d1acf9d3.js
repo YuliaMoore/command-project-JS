dbf048dc0ccd431a872be0d9425e88c59667a96d
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-05-07 14:10:55 +0000

    Deploying to gh-pages from @ YuliaMoore/command-project-JS@f22c2c988873114441c74b6009b9bcebaaf539e3 🚀

--- catalog.d1acf9d3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e;(e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")}).openMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}))}();
-//# sourceMappingURL=catalog.d1acf9d3.js.map
